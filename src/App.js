62a083ed49c5d71c6397748674a4a5ecb60b5374
Nilay Singh <snilay011@gmail.com>
2022-06-06 01:53:28 +0530

    Login and register page done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
 
 
 function App() {
   return (
   <>
     <Topbar/>
-    <Settings/>
+    <Register/>
   </>
 
   );
